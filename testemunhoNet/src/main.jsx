e0ec3c6547a0b944e3f6df4c569f2d620e36d623
eduardooarruda <77410740+eduardooarruda@users.noreply.github.com>
2023-10-02 17:20:42 -0300

    organizei as rotas de outra forma

--- testemunhoNet/src/main.jsx
+++ testemunhoNet/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
