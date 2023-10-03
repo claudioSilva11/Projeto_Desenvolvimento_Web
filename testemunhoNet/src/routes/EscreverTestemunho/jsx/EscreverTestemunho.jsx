727879a31d121a4c77fde9058e2e160a81dd6802
luana <luannamaria8562gmail.com>
2023-10-03 00:42:33 -0300

    Mudei a cor da borda e o espaçamento dos input d radios

--- testemunhoNet/src/routes/EscreverTestemunho/jsx/EscreverTestemunho.jsx
+++ testemunhoNet/src/routes/EscreverTestemunho/jsx/EscreverTestemunho.jsx
@@ -7,14 +7,14 @@ const EscreverTestemunho = () => {
             <div id="formulario">
                 <form action="#" method="get">
                     <label for="titulo">Escolha um titulo:</label><br /><br />
-                    <input type="titulo" id="titulo" name="titulo" /><br /><br />
+                    <input type="titulo" id="titulo" name="titulo" className="form-control"/><br /><br />
                     <label for="testemunho">Escreva o seu testemunho:</label><br /><br />
-                    <textarea id="testemunho" name="testemunho"> </textarea><br /><br />
+                    <textarea id="testemunho" name="testemunho" className="form-control" > </textarea><br /><br />
                     <label for="comentarios">Comentários na publicação:</label><br /><br />
                     <input type="radio" id="ativado" name="comentarios" value="ativado" />
                     <label id="ativado"> Ativado </label>
                     <input type="radio" id="desativado" name="comentarios" value="desativado" />
-                    <label id="desativado">Desativado </label> <br /><br/>
+                    <label for="desativado">Desativado </label> <br /><br/>
                     <input type="submit" value="Publicar" id="enviar" className="btn" />
                 </form>
             </div>
