e5dca335c14ff4b795401170087e45fd93a7011b
luana <luannamaria8562gmail.com>
2023-10-03 00:28:11 -0300

    Pagina para escrever o testemunho

--- testemunhoNet/src/routes/EscreverTestemunho/jsx/EscreverTestemunho.jsx
+++ testemunhoNet/src/routes/EscreverTestemunho/jsx/EscreverTestemunho.jsx
@@ -3,40 +3,22 @@ import "../css/escreverTestemunho.css";
 
 const EscreverTestemunho = () => {
     return (
-        <div className="escrever-container">
-            <div className="escrever-container__conteudo">
-                <form>
-                    <div className="escrever-container__item escrever-container__entrada escrever-container__entrada-titulo">
-                        <p>Escolha um título:</p>
-                        <input type="text" className="form-control" aria-label="Username" aria-describedby="basic-addon1"></input>
-                    </div>
-                    <div className="escrever-container__item escrever-container__entrada escrever-container__entrada-conteudo">
-                        <p>Escreva o seu testemunho:</p>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    <div className="escrever-container__item escrever-container__publicacao">
-                        <p>Comentários na publicação:</p>
-                        <div className="publicacao__container">
-                            <div className="form-check">
-                                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
-                                <label className="form-check-label" >
-                                    Ativado
-                                </label>
-                            </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
-                                <label className="form-check-label">
-                                    Desativado
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="escrever-container__item escrever-container__button">
-                        <Button classe="publicacao__button">Publicar</Button>
-                    </div>
+        <>
+            <div id="formulario">
+                <form action="#" method="get">
+                    <label for="titulo">Escolha um titulo:</label><br /><br />
+                    <input type="titulo" id="titulo" name="titulo" /><br /><br />
+                    <label for="testemunho">Escreva o seu testemunho:</label><br /><br />
+                    <textarea id="testemunho" name="testemunho"> </textarea><br /><br />
+                    <label for="comentarios">Comentários na publicação:</label><br /><br />
+                    <input type="radio" id="ativado" name="comentarios" value="ativado" />
+                    <label id="ativado"> Ativado </label>
+                    <input type="radio" id="desativado" name="comentarios" value="desativado" />
+                    <label id="desativado">Desativado </label> <br /><br/>
+                    <input type="submit" value="Publicar" id="enviar" className="btn" />
                 </form>
             </div>
-        </div>
+        </>
     );
 }
 
